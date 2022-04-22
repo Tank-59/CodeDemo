d9f60e70cf4d5924eb352ae36f2ee4fd1eccb3ab
CaoYanqi <caoyanqi15@126.com>
2022-04-22 11:00:45 +0800

    gym - push

---
+++ TypeScript-类型体操/src/03057-easy-push.ts
@@ -0,0 +1,14 @@
+// 在类型系统里实现通用的 Array.push 
+// eg:type Result = Push<[1, 2], '3'> // [1, 2, '3']
+
+type Push<T extends any[], U> = [...T, U]
+
+
+// test case
+//-----------------------------------------------------------------------------
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Push<[], 1>, [1]>>,
+  Expect<Equal<Push<[1, 2], '3'>, [1, 2, '3']>>,
+  Expect<Equal<Push<['1', 2, '3'], boolean>, ['1', 2, '3', boolean]>>,
