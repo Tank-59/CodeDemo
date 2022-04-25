8ea1207fd5314e16e8994bfdde5f381fb8949965
CaoYanqi <caoyanqi15@126.com>
2022-04-25 17:36:52 +0800

    gym- return type

---
+++ TypeScript-类型体操/src/00002-mid-return-type.ts
@@ -0,0 +1,29 @@
+// 描述：不使用 ReturnType 实现 TypeScript 的 ReturnType<T> 泛型。
+// eg：
+
+//实现：
+type MyReturnType<T> = T extends (...args: any[]) => infer R ? R : never
+
+
+
+//------ test case --------------
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<string, MyReturnType<() => string>>>,
+  Expect<Equal<123, MyReturnType<() => 123>>>,
+  Expect<Equal<ComplexObject, MyReturnType<() => ComplexObject>>>,
+  Expect<Equal<Promise<boolean>, MyReturnType<() => Promise<boolean>>>>,
+  Expect<Equal<() => 'foo', MyReturnType<() => () => 'foo'>>>,
+  Expect<Equal<1 | 2, MyReturnType<typeof fn>>>,
+  Expect<Equal<1 | 2, MyReturnType<typeof fn1>>>,
+]
+
+type ComplexObject = {
+  a: [12, 'foo']
+  bar: 'hello'
+  prev(): number
+}
+
+const fn = (v: boolean) => v ? 1 : 2
+const fn1 = (v: boolean, w: any) => v ? 1 : 2
